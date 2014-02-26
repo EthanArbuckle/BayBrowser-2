f85ef79d545ee1e32b124a4588a1e0d8bc4edeff
ethanarb <ethan.a.arbuckle@gmail.com>
2014-02-25 19:29:54 -0600

    Began Network Overlay

---
+++ BayBrowser 2/Objects/EANetworkSpeesOverlay.h
@@ -0,0 +1,13 @@
+//
+//  EANetworkSpeesOverlay.h
+//  BayBrowser 2
+//
+//  Created by Ethan Arbuckle on 2/25/14.
+//  Copyright (c) 2014 Ethan Arbuckle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EANetworkSpeesOverlay : UIView
+
+@end
